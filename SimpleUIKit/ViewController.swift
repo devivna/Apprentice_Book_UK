723e780284dc28cddb198b9e016997615d9804c7
Nadia <ivashchenko.nadia@gmail.com>
2022-12-23 22:08:12 +0200

    add labels, slider and buttons

--- SimpleUIKit/ViewController.swift
+++ SimpleUIKit/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 50
+    
+    @IBOutlet weak var headerText: UILabel!
+    
+    @IBOutlet weak var headerNumber: UILabel!
+    
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var sliderMinValueLabel: UILabel!
+    
+    @IBOutlet weak var sliderMaxValueLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var scoreTextLabel: UILabel!
+    
+    @IBOutlet weak var scoreNumberLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var roundNumberLabel: UILabel!
+    
+    @IBOutlet weak var roundTextLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+        
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lround(Double(slider.value))
+        //print("The value of the slider is now: \(slider.value)")
+        
+    }
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(
+            title: "Excelent!",
+            message: message,
+            preferredStyle: .alert)
+        
+        let button = UIAlertAction(
+            title: "OK",
+            style: .default)
 
+        alert.addAction(button)
+        present(alert, animated: true)
+        
+    }
+
+    @IBAction func infoButton(_ sender: UIButton) {
+    }
+    
 
 }
 
